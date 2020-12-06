595db0854372afad28c4123f223695b62ca62f44
Okhunov Odil <odiljon.ahunov.1997@mail.ru>
2020-12-07 02:12:10 +0900

    Initial commit

--- SeeFood CoreML/ViewController.swift
+++ SeeFood CoreML/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
-
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            detect(image: ciimage )
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading Core ML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+           
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "HotDog!"
+                }else{
+                    self.navigationItem.title = "Not HotDog!"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+        try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+    
 }
 
