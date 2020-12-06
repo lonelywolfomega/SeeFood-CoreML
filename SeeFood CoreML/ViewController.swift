e88467336ba34bebd72178b72352f5330d7f329a
Okhunov Odil <odiljon.ahunov.1997@mail.ru>
2020-12-07 01:05:49 +0900

    Initial Commit

---
+++ SeeFood CoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood CoreML
+//
+//  Created by Okhunov Odil on 2020/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
